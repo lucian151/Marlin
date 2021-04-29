6548dc3a3e472db25de5310fe20ed62bae49d7e4
lucian <lucianchapar@gmail.com>
2021-04-29 12:34:39 -0400

    Initial Release

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -343,7 +343,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 185
+#define X_MAX_POS 145
 #define X_MIN_POS 0
 #define Y_MAX_POS 155
 #define Y_MIN_POS 0
@@ -488,7 +488,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,(78.7402*(100/39.15)),200.0*8/3,760*1.1}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402*100/78.7,(78.7402*(100/39.15)),200.0*8/3*50/67,760*1.1}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 20, 50}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
